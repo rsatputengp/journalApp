496c27ee4b2362cadbe398f26a8729cdbaa27fc2
rsatputengp <rsatputengp@gmail.com>
2025-06-06 15:22:20 +0530

    Add the @Service

--- src/main/java/com/springx/mypro/service/UserService.java
+++ src/main/java/com/springx/mypro/service/UserService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -17,7 +18,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-@Component
+@Service
 @Slf4j
 public class UserService {
 
--- src/main/java/com/springx/mypro/service/WeatherService.java
+++ src/main/java/com/springx/mypro/service/WeatherService.java
@@ -5,9 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherService {
 
     private final String apikey = "447c6394ca8637765fe64aaa174870ef";
