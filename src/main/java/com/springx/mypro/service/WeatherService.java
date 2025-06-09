10302e001e938f6788633ecf62dfba535e01bdd4
rsatputengp <rsatputengp@gmail.com>
2025-06-09 15:41:34 +0530

    use @Value without static because when you use static in @Value then you will get null point exception that's why proper way to use @Value without static key word

--- src/main/java/com/springx/mypro/service/WeatherService.java
+++ src/main/java/com/springx/mypro/service/WeatherService.java
@@ -2,6 +2,7 @@ package com.springx.mypro.service;
 
 import com.springx.mypro.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -11,9 +12,10 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherService {
 
-    private final String apikey = "447c6394ca8637765fe64aaa174870ef";
+    @Value("${weather.api.key}")
+    private String apikey;
 
-    private final String url = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
+    private static final String url = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
     @Autowired
     private RestTemplate restTemplate;
