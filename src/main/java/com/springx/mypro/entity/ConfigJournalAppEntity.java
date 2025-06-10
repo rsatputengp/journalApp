432da04dfcaf8907b0de5c80ace062869f0f9185
rsatputengp <rsatputengp@gmail.com>
2025-06-10 15:50:04 +0530

    key, url are stored in database and get in hasMap ( Map<String, String> appCache ) Cache and clear Cache in runtime using @PostConstruct

--- src/main/java/com/springx/mypro/entity/ConfigJournalAppEntity.java
+++ src/main/java/com/springx/mypro/entity/ConfigJournalAppEntity.java
@@ -2,11 +2,6 @@ package com.springx.mypro.entity;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.NonNull;
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
--- src/main/java/com/springx/mypro/repository/ConfigJournalAppRepository.java
+++ src/main/java/com/springx/mypro/repository/ConfigJournalAppRepository.java
@@ -1,12 +1,12 @@
 package com.springx.mypro.repository;
 
 import com.springx.mypro.entity.ConfigJournalAppEntity;
-import com.springx.mypro.entity.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Component;
 
-@Component
+import java.util.List;
+
 public interface ConfigJournalAppRepository extends MongoRepository<ConfigJournalAppEntity, ObjectId> {
 
+    List<ConfigJournalAppEntity> findAll();
 }
