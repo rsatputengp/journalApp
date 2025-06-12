9e5e745d161f6b7d5ffda4a17f3bebe1b7eee379
rsatputengp <rsatputengp@gmail.com>
2025-06-12 12:28:48 +0530

    JavaMailSender is added in code

--- src/test/java/com/springx/mypro/service/EmailServiceTests.java
+++ src/test/java/com/springx/mypro/service/EmailServiceTests.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class EmailServiceTests {
+class EmailServiceTests {
 
     @Autowired
     private EmailService emailService;
