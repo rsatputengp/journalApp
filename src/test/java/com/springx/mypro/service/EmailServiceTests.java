0c96167f2b037d03372a89e723e26301b753ed2f
rsatputengp <rsatputengp@gmail.com>
2025-06-12 14:14:08 +0530

    JavaMailSender is added in code

--- src/test/java/com/springx/mypro/service/EmailServiceTests.java
+++ src/test/java/com/springx/mypro/service/EmailServiceTests.java
@@ -4,6 +4,10 @@ package com.springx.mypro.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class EmailServiceTests {
@@ -11,10 +15,18 @@ class EmailServiceTests {
     @Autowired
     private EmailService emailService;
 
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+
     @Test
     void testSendEmail() {
         emailService.sendEmail("rsatputengp@gmail.com",
                 "Testing Java Email sender",
                   "Yoo Shinchan {^_^}");
+
+//        // ✅ Proper verification
+//        verify(javaMailSender, times(1)).send(any(SimpleMailMessage.class));
+
     }
 }
