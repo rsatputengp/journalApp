472a6d150a194a89ba77af9c4a2c761e93bbfd1c
rsatputengp <rsatputengp@gmail.com>
2025-06-12 15:11:31 +0530

    updated code

--- src/test/java/com/springx/mypro/service/EmailServiceTests.java
+++ src/test/java/com/springx/mypro/service/EmailServiceTests.java
@@ -1,24 +1,24 @@
 package com.springx.mypro.service;
 
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import static org.mockito.Mockito.*;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import static org.mockito.Mockito.*;
 
-@SpringBootTest
+//@SpringBootTest
 class EmailServiceTests {
-
-    @Autowired
-    private EmailService emailService;
-
-    @MockBean
-    private JavaMailSender javaMailSender;
-
-
+//
+//    @Autowired
+//    private EmailService emailService;
+//
+//    @MockBean
+//    private JavaMailSender javaMailSender;
+//
+//
 //    @Test
 //    void testSendEmail() {
 //        emailService.sendEmail("rsatputengp@gmail.com",
